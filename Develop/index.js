407e4709c6db8f2af06d7ad153021751a0635dcf
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-29 20:26:36 -0500

    didn't save the array input from last time, pushing again

--- Develop/index.js
+++ Develop/index.js
@@ -9,8 +9,35 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Application title?'
+    },
+
+    {
+        type: 'input',
+        name: 'github',
+        message: "Enter your github username please."
+    },
+
+    {
+        type: 'input',
+        name: 'repo',
+        message: 'Enter the name of your github repo.'
+    },
+
+    {
+        
+    }
+];
+
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
