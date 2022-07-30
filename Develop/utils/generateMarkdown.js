399466b47586a88b07fea2f67676465644149dff
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-30 12:05:41 -0500

    added license link function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,7 @@
 // If there is no license, return an empty string
 
 function renderLicenseBadge(license) {
+  
   //Return an empty string
   if(!license) {
     return '';
@@ -14,12 +15,28 @@ function renderLicenseBadge(license) {
   } else if (license === 'Apache') {
     return `![AUR license](https://img.shields.io/aur/license/readme)`
   }
+
 };
 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+  //Return an empty string
+  if (!license) {
+    return '';
+
+    //Return MIT license link
+  } if (license === 'MIT') {
+    return `[View MIT license link](https://opensource.org/licenses/MIT)`
+    
+    //Return Apache license link
+  } if (license === 'Apache') {
+    return `[View Apache license link](https://www.apache.org/licenses/LICENSE-2.0)`
+  }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
